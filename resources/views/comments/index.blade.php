082da0630ebf581941e0db46347623f21f73949d
Segara Bhagas Dagsapurwa <segara2410@gmail.com>
2020-07-19 21:13:29 +0700

    minor

--- resources/views/comments/index.blade.php
+++ resources/views/comments/index.blade.php
@@ -12,9 +12,6 @@
                             <div class="col-8">
                                 <h3 class="mb-0">My Comments ({{ $comments->count() }})</h3>
                             </div>
-                            <div class="col-4 text-right">
-                                <a href="/post/create" class="btn btn-sm btn-primary">Write Post</a>
-                            </div>
                         </div>
                     </div>
                     @if (session('status'))
